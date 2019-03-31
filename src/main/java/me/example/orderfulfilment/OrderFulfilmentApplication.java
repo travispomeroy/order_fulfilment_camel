cfcb5c769b3d02232fb3ba612fb8d46c1d84d58e
Travis Pomeroy <travis.pomeroy84@gmail.com>
2019-03-31 09:12:16 -0400

    Initial Commit. Added liquibase changes to crate basic order tables.

---
+++ src/main/java/me/example/orderfulfilment/OrderFulfilmentApplication.java
@@ -0,0 +1,13 @@
+package me.example.orderfulfilment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFulfilmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFulfilmentApplication.class, args);
+    }
+
+}
